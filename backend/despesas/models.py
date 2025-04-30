f13ce94de15d4b8e4fa0c743d5d951c3535054a6
LuisF3L1P3dev <dantas.luis@escolar.ifrn.edu.br>
2025-04-30 16:11:41 -0300

    create model Despesa

--- backend/despesas/models.py
+++ backend/despesas/models.py
@@ -6,4 +6,12 @@ class Categoria(models.Model):
 
     def __str__(self):
         return self.nome
-    
+
+class Despesa(models.Model):
+    descricao = models.Charfild(max_length=100)
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+    data = models.DateField()
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return  "descrição: {}, valor:{}, data: {}, catergoria: {}".format(self.descricao, self.valor, self.data, self.categoria.nome)
