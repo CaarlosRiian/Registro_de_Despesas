24f8078a9853087573d768de27cc826c9f80f47c
LuisF3L1P3dev <dantas.luis@escolar.ifrn.edu.br>
2025-04-30 16:54:52 -0300

    criacao da dispesa serializers

--- backend/despesas/serializers.py
+++ backend/despesas/serializers.py
@@ -5,3 +5,16 @@ class CategoriaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Categoria
         fields = '__all__'
+
+class DespesaSerializer(serializers.ModelSerializer):
+    categoria = CategoriaSerializer(read_only=True)
+    categoria_id = serializers.PrimaryKeyRelatedField(
+        queryset=Categoria.objects.all(),
+        source='categoria',
+        write_only=True
+    )
+
+    class Meta:
+        model = Despesa
+        fields = "__all__"
+         
