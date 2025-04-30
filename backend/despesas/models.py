90f62bea589333f883d854acc1eeaea73b8dc07f
LuisF3L1P3dev <dantas.luis@escolar.ifrn.edu.br>
2025-04-30 16:28:46 -0300

    correcao dos models e relizado makemigrations

--- backend/despesas/models.py
+++ backend/despesas/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Categoria(models.Model):
-    nome = models.CharField(max_langht = 50, unique=True)
+    nome = models.CharField(max_length=50, unique=True)
 
     def __str__(self):
         return self.nome
 
 class Despesa(models.Model):
-    descricao = models.Charfild(max_length=100)
+    descricao = models.CharField(max_length=100)
     valor = models.DecimalField(max_digits=10, decimal_places=2)
     data = models.DateField()
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
