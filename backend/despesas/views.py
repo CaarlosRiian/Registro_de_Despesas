fd998b153ac901204b01b16039d2c457ac43d890
LuisF3L1P3dev <dantas.luis@escolar.ifrn.edu.br>
2025-05-01 13:29:39 -0300

    criacao de viewset catergoria e despesa

--- backend/despesas/views.py
+++ backend/despesas/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from .models import Despesa, Categoria
+from .serializers import DespesaSerializer, CategoriaSerializer
 # Create your views here.
+
+class CategotiaViewSet(videwsets.ModelViewSet):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
+    
+class DespesaViewSet(viewsets.ModelViewSet):
+    queryset = Despesa.objects.all()
+    serializer_class = DespesaSerializer
+
+
