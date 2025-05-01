d9911b5ca324123d37e619a6e79d5001595c2f29
LuisF3L1P3dev <dantas.luis@escolar.ifrn.edu.br>
2025-05-01 14:43:51 -0300

    fix: alterado nome da rota para api

--- backend/registro_de_despesas/urls.py
+++ backend/registro_de_despesas/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('despesas/', include("despesas.urls")),
+    path('api/', include("despesas.urls")),
 ]
