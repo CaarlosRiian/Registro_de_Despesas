6528b1878dd51a08e97e688c58499cb73ee8b22e
caarlosriian <rlina1785@gmail.com>
2025-05-04 13:10:23 -0300

    style: estilizado pagina de cadastro da categoria

--- frontend/src/components/categoryForm.jsx
+++ frontend/src/components/categoryForm.jsx
@@ -1,20 +1,26 @@
-import { use, useState } from "react"
+import { use, useEffect, useState } from "react"
 import axios from "axios"
 import { useNavigate } from "react-router-dom"
-import styles from "../styles/categoryForm.css"
+import styles from "../styles/categoryForm.module.css"
 
 function CategoryForm () {
     const [nome, setNome] = useState("")
     const [mensagem, setMensagem] = useState(null)
     const navigate = useNavigate()
 
+    useEffect(() => {
+        document.body.classList.add(styles.customBody)
+        return () => {
+            document.body.classList.remove(styles.customBody)
+        }
+    }, [])
+
     const handleSubmit = async (e) => {
         e.preventDefault()
 
         try {
             await axios.post("http://localhost:8000/api/categorias/", {nome})
-            setMensagem("Categoria Cadastrada com Sucesso!")
-            setNome("")
+            navigate("/")
         } catch (error) {
             setMensagem("Erro ao cadastrar a Categoria!")
             console.log(error)
@@ -27,23 +33,24 @@ function CategoryForm () {
 
     return (
         <>
-            <div style={{ padding: "1rem" }}>
-            <h2>Cadastrar Categoria</h2>
-
-            <form onSubmit={handleSubmit}>
-                <input
-                type="text"
-                placeholder="Nome da categoria"
-                value={nome}
-                onChange={(e) => setNome(e.target.value)}
-                required
-                />
-                <button type="submit">Cadastrar</button>
-            </form>
-
-            {mensagem && <p>{mensagem}</p>}
+        
+            <div className={styles["container-div"]}>
+                <h1 className={styles["h2-form-category"]}>Cadastrar Categoria</h1>
+                <div className={styles["form-category-style"]}>
+                    <form onSubmit={handleSubmit}>
+                        <input
+                        type="text"
+                        className={styles["input-form-category"]}
+                        placeholder="Nome da categoria"
+                        value={nome}
+                        onChange={(e) => setNome(e.target.value)}
+                        required
+                        />
+                        <button className={styles["button-form-category-register"]} type="submit">Cadastrar</button>
+                    </form>
+                </div>
+                <button className={styles["button-back-home-form-category"]} onClick={handleBack}>Voltar</button>
             </div>
-            <button onClick={handleBack}>Voltar</button>
         </>
     )
 }
