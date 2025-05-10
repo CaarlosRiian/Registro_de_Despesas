5998caa2f9a43157f63640dceb5c7d2fe8594a14
caarlosriian <rlina1785@gmail.com>
2025-05-10 20:34:58 -0300

    feat: Adicionando listagem de categoria

---
+++ frontend/src/components/categoryList.jsx
@@ -0,0 +1,76 @@
+import { useState, useEffect } from "react"
+import axios from "axios"
+import { useNavigate } from "react-router-dom"
+import styles from "../styles/categoryList.module.css"
+
+function CategoryList() {
+    const [categorias, setCategorias] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(null)
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        document.body.classList.add(styles.customBody)
+            return () => {
+                document.body.classList.remove(styles.customBody)
+            }
+    }, [])
+
+
+    useEffect(() => {
+        async function fetchCategorias() {
+            try {
+                const response = await axios.get("http://localhost:8000/api/categorias/")
+                setCategorias(response.data)
+            } catch (err) {
+                setError("Erro ao carregar as categorias.")
+            } finally {
+                setLoading(false)
+            }
+        }
+
+        fetchCategorias()
+    }, [])
+
+    const handleDelete = async (id) => {
+        if (window.confirm("Deseja excluir esta categoria?")) {
+            try {
+                await axios.delete(`http://localhost:8000/api/categorias/${id}/`)
+                setCategorias(categorias.filter(categoria => categoria.id !== id))
+            } catch (err) {
+                alert("Erro ao excluir a categoria.")
+            }
+        }
+    }
+
+    if (loading) return <p>Carregando categorias...</p>
+    if (error) return <p>{error}</p>
+
+    const handleBack = async () => {
+        navigate("/")
+    }
+    
+
+    return (
+        <>
+
+            <button onClick={handleBack} className={styles["button-back-in-list-category"]}>Voltar</button>
+            <div className={styles["div-list-category"]}>
+                <h1 className={styles["h1-list-category"]}>Lista de Categorias</h1>
+                <ul className={styles.list}>
+                    {categorias.map(categoria => (
+                        <li key={categoria.id} className={styles.item}>
+                            {categoria.nome}
+                            <button
+                                onClick={() => handleDelete(categoria.id)}
+                                className={styles["button-delete-in-list-form-category"]}
+                            >Excluir</button>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </>
+    )
+}
+
+export default CategoryList
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,7 @@ import App from './App.jsx'
 import CategoryForm from './components/categoryForm'
 import ExpenseForm from './components/expenseForm'
 import EditExpense from './components/editExpense'
+import CategoryList from './components/categoryList.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -12,6 +13,7 @@ createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/categorias" element={<CategoryForm />} />
+        <Route path="/listar_despesa" element={<CategoryList/>} />
         <Route path="/cadastrar_despesa" element={<ExpenseForm/>} />
         <Route path="/editar/:id" element={<EditExpense/>} />
       </Routes>
